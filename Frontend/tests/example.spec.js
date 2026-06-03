101876bed38ac4186fc8773e5991cbfcc6999427
Astrum2 <jpn.agostinho27@gmail.com>
2026-06-02 22:28:57 -0300

    feat: Teste end-to-end Cadsatro

--- Frontend/tests/example.spec.js
+++ Frontend/tests/example.spec.js
@@ -3,13 +3,7 @@ import { test, expect } from '@playwright/test';
 
 test('login tem sucesso e redireciona para a home', async ({ page }) => {
   await page.route('http://localhost:3001/login', async (route) => {
-    await route.fulfill({
-      status: 200,
-      contentType: 'application/json',
-      body: JSON.stringify({
-        token: 'fake.jwt.token'
-      }),
-    });
+    await route.fulfill({status: 200, contentType: 'application/json', body: JSON.stringify({ token: 'fake.jwt.token'})});
   });
 
   await page.goto('http://localhost:3000/#/Login');
@@ -41,4 +35,43 @@ test('login falha e mostra mensagem de erro', async ({ page }) => {
 
   await expect(page.getByRole('alert')).toHaveText('Email ou senha inválidos');
   await expect(page).toHaveURL('http://localhost:3000/#/Login');
+});
+
+test('cadastro tem sucesso e redireciona para a home', async ({ page }) => {
+  await page.route('http://localhost:3001/users', async (route) => {
+    await route.fulfill({ status: 201, contentType: 'application/json', body: JSON.stringify({}) });
+  });
+  await page.route('http://localhost:3001/login', async (route) => {
+    await route.fulfill({ status: 200, contentType: 'application/json', body: JSON.stringify({ token: 'fake.jwt.token' })});
+  });
+
+  await page.goto('http://localhost:3000/#/Cadastro');
+
+  await page.getByLabel('Nome:').fill('Usuário Teste');
+  await page.getByLabel('Email:').fill('usuario.test@example.com');
+  await page.getByLabel('Senha:').nth(0).fill('Abc123#');
+  await page.getByLabel('Confirmar Senha:').fill('Abc123#');
+  await page.getByLabel('CPF:').fill('923.332.930-55');
+  await page.getByRole('button', { name: 'Cadastrar' }).click();
+
+  await expect(page.getByRole('alert')).toHaveText('Cadastro realizado com sucesso!');
+  await expect(page).toHaveURL('http://localhost:3000/#/');
+});
+
+test('cadastro falha e mostra mensagem de erro', async ({ page }) => {
+  await page.route('http://localhost:3001/users', async (route) => {
+    await route.fulfill({ status: 400, contentType: 'application/json', body: JSON.stringify({ message: 'Email já cadastrado' }) });
+  });
+
+  await page.goto('http://localhost:3000/#/Cadastro');
+
+  await page.getByLabel('Nome:').fill('Usuário Teste');
+  await page.getByLabel('Email:').fill('usuario.test@example.com');
+  await page.getByLabel('Senha:').nth(0).fill('Abc123#');
+  await page.getByLabel('Confirmar Senha:').fill('Abc123#');
+  await page.getByLabel('CPF:').fill('923.332.930-55');
+  await page.getByRole('button', { name: 'Cadastrar' }).click();
+
+  await expect(page.getByRole('alert')).toHaveText('Email já cadastrado');
+  await expect(page).toHaveURL('http://localhost:3000/#/Cadastro');
 });
